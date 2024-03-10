6dbc98db95395ef71d48d11ed82a72c4d101c93e
marc Springer <hackcheck78@gmail.com>
2024-03-10 20:30:18 +0100

    removed unused import in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      envFilePath: '.env',
-    })
-  ],
+    ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [],
 })
